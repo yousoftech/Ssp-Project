51cb34ba8658849b52ecc5fe4d75803fb345262c
Kush <kushshah1234.ks@gmail.com>
2017-12-20 17:50:10 +0530

    commite with admin.

--- app/src/main/java/com/ssp/Fragment/YatriFragment.java
+++ app/src/main/java/com/ssp/Fragment/YatriFragment.java
@@ -2,6 +2,7 @@ package com.ssp.Fragment;
 
 
 import android.app.ProgressDialog;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
@@ -10,6 +11,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.android.volley.DefaultRetryPolicy;
 import com.android.volley.Request;
@@ -21,6 +23,7 @@ import com.android.volley.toolbox.Volley;
 import com.ssp.Adapter.adapterYatra;
 import com.ssp.Model.beanYatra;
 import com.ssp.R;
+import com.ssp.Util.ConnectionDetector;
 import com.ssp.Util.Constant;
 
 import org.json.JSONArray;
@@ -28,6 +31,9 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
+
+import static android.content.Context.MODE_PRIVATE;
+import static com.ssp.Activity.LoginActivity.PREFS_NAME;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -38,7 +44,10 @@ public class YatriFragment extends Fragment {
     ProgressDialog progressDialog;
     beanYatra yatra;
     ArrayList<beanYatra> event;
+    ConnectionDetector detector;
     adapterYatra aYatra;
+    SharedPreferences preferences;
+    int userId;
 
     public YatriFragment() {
         // Required empty public constructor
@@ -50,69 +59,73 @@ public class YatriFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_yatri, container, false);
+        detector = new ConnectionDetector(getContext());
+        preferences = preferences = getContext().getSharedPreferences(PREFS_NAME, MODE_PRIVATE);
         recyclerView = (RecyclerView) view.findViewById(R.id.recylerViewYatri);
-        event = new ArrayList<beanYatra>(
-
-        );
+        userId = preferences.getInt("userId", 0);
+        event = new ArrayList<beanYatra>();
         detailYatra();
         return view;
     }
 
     public void detailYatra() {
-        progressDialog = new ProgressDialog(getContext());
-        progressDialog.setCancelable(false);
-        progressDialog.setMessage("Loading...");
-        progressDialog.show();
+        if (detector.isConnectingToInternet()) {
+            progressDialog = new ProgressDialog(getContext());
+            progressDialog.setCancelable(false);
+            progressDialog.setMessage("Loading...");
+            progressDialog.show();
 
-        JsonObjectRequest objectRequest = new JsonObjectRequest(Request.Method.GET,
-                Constant.PATH + "Spot/getspotdetailsbyyatri?iUserDetailsId=3", null, new Response.Listener<JSONObject>() {
-            @Override
-            public void onResponse(JSONObject response) {
-                Log.d("yatra", response.toString());
-                try {
-                    boolean code = response.getBoolean("status");
-                    if (code == true) {
-                        progressDialog.dismiss();
-                        JSONArray array = response.getJSONArray("data");
-                        Log.d("yatra", array.toString());
-                        for (int n=0;n<array.length();n++){
-                            JSONObject obj = array.getJSONObject(n);
-                            yatra=new beanYatra();
-                            int yatraNo=obj.getInt("iYatraNo");
-                            int spotNo = obj.getInt("iSpotNo");
-                            String dateTime=obj.getString("datetimeYatraTime");
-                            String upDown = obj.getString("strUpOrDown");
-                            yatra.setYatraUpDown(upDown);
-                            yatra.setYatraNo(yatraNo);
-                            yatra.setSpotNo(spotNo);
-                            yatra.setYatraTime(dateTime);
-                            event.add(yatra);
-                            aYatra = new adapterYatra(getContext(), event);
-                            recyclerView.setAdapter(aYatra);
-                            recyclerView.setLayoutManager(new LinearLayoutManager(getContext(), LinearLayoutManager.VERTICAL, false));
+            JsonObjectRequest objectRequest = new JsonObjectRequest(Request.Method.GET,
+                    Constant.PATH + "Spot/getspotdetailsbyyatri?iUserDetailsId=" + userId, null, new Response.Listener<JSONObject>() {
+                @Override
+                public void onResponse(JSONObject response) {
+                    Log.d("yatra", response.toString());
+                    try {
+                        boolean code = response.getBoolean("status");
+                        if (code == true) {
                             progressDialog.dismiss();
+                            JSONArray array = response.getJSONArray("data");
+                            Log.d("yatra", array.toString());
+                            for (int n = 0; n < array.length(); n++) {
+                                JSONObject obj = array.getJSONObject(n);
+                                yatra = new beanYatra();
+                                int yatraNo = obj.getInt("iYatraNo");
+                                int spotNo = obj.getInt("iSpotNo");
+                                String dateTime = obj.getString("datetimeYatraTime");
+                                String upDown = obj.getString("strUpOrDown");
+                                yatra.setYatraUpDown(upDown);
+                                yatra.setYatraNo(yatraNo);
+                                yatra.setSpotNo(spotNo);
+                                yatra.setYatraTime(dateTime);
+                                event.add(yatra);
+                                aYatra = new adapterYatra(getContext(), event);
+                                recyclerView.setAdapter(aYatra);
+                                recyclerView.setLayoutManager(new LinearLayoutManager(getContext(), LinearLayoutManager.VERTICAL, false));
+                                progressDialog.dismiss();
+                            }
                         }
+
+                    } catch (JSONException e) {
+                        e.printStackTrace();
                     }
 
-                } catch (JSONException e) {
-                    e.printStackTrace();
                 }
+            }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
 
-            }
-        }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-
-                if (progressDialog != null)
-                    progressDialog.dismiss();
-            }
-        });
-        objectRequest.setRetryPolicy(new DefaultRetryPolicy(
-                30000,
-                DefaultRetryPolicy.DEFAULT_MAX_RETRIES,
-                DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
-        RequestQueue requestQueue = Volley.newRequestQueue(getContext());
-        requestQueue.add(objectRequest);
+                    if (progressDialog != null)
+                        progressDialog.dismiss();
+                }
+            });
+            objectRequest.setRetryPolicy(new DefaultRetryPolicy(
+                    30000,
+                    DefaultRetryPolicy.DEFAULT_MAX_RETRIES,
+                    DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
+            RequestQueue requestQueue = Volley.newRequestQueue(getContext());
+            requestQueue.add(objectRequest);
+        } else {
+            Toast.makeText(getContext(), "Please check your internet connection before verification..!", Toast.LENGTH_LONG).show();
+        }
     }
-
 }
---
+++ app/src/main/java/com/ssp/Model/yatriNumber.java
@@ -0,0 +1,18 @@
+package com.ssp.Model;
+
+/**
+ * Created by DELL on 12/20/2017.
+ */
+
+public class yatriNumber {
+
+    int YatriNumber;
+
+    public int getYatriNumber() {
+        return YatriNumber;
+    }
+
+    public void setYatriNumber(int yatriNumber) {
+        YatriNumber = yatriNumber;
+    }
+}
