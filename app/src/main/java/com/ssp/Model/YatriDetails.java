b1c49990577eb18c4e478048ac201d5f13cf075c
yousoftech <arpitshah@yousoftech.com>
2017-12-24 02:20:45 +0530

    Change Password Successfully Done

---
+++ app/src/main/java/com/ssp/Model/YatriDetails.java
@@ -0,0 +1,67 @@
+package com.ssp.Model;
+
+/**
+ * Created by Admin on 24-12-2017.
+ */
+
+public class YatriDetails {
+        public String strUserCode;
+    public int iUserDetailsId;
+    public int iSpotId;
+    public int iYatraNo;
+    public String strUserName;
+    public String strUpOrDown;
+
+    public String getStrUserCode() {
+        return strUserCode;
+    }
+
+    public void setStrUserCode(String strUserCode) {
+        this.strUserCode = strUserCode;
+    }
+
+    public int getiUserDetailsId() {
+        return iUserDetailsId;
+    }
+
+    public void setiUserDetailsId(int iUserDetailsId) {
+        this.iUserDetailsId = iUserDetailsId;
+    }
+
+    public int getiSpotId() {
+        return iSpotId;
+    }
+
+    public void setiSpotId(int iSpotId) {
+        this.iSpotId = iSpotId;
+    }
+
+    public int getiYatraNo() {
+        return iYatraNo;
+    }
+
+    public void setiYatraNo(int iYatraNo) {
+        this.iYatraNo = iYatraNo;
+    }
+
+    public String getStrUserName() {
+        return strUserName;
+    }
+
+    public void setStrUserName(String strUserName) {
+        this.strUserName = strUserName;
+    }
+
+    public String getStrUpOrDown() {
+        return strUpOrDown;
+    }
+
+    public void setStrUpOrDown(String strUpOrDown) {
+        this.strUpOrDown = strUpOrDown;
+    }
+
+
+
+    }
+
+
